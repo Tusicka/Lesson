10e968a778500eac52bb1c7506b2210edb1bb0b7
Danylo <jeterojet@gmail.com>
2017-03-19 16:16:52 +0200

    fdf

---
+++ Assets/Script/CubeControls.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeControls : MonoBehaviour {
+    public Rigidbody rb;
+    public int Speed;
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical);
+        rb.AddForce(direction * Speed);
+	}
+}
